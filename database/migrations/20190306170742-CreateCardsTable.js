93fb902374d4cbd9cd1dbd730891b62e227ee171
BlackHades <michealakinwonmi@gmail.com>
2019-03-06 18:11:11 +0100

    Cards Migration Created

---
+++ database/migrations/20190306170742-CreateCardsTable.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+
+    return queryInterface.createTable('cards', {
+      id: {
+        type: type.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: type.STRING,
+      country: type.STRING,
+      description: {
+        type: type.TEXT,
+        allowNull: true
+      },
+      type: {
+        type: type.STRING,
+        allowNull: true,
+      },
+      priceRange:{
+        type: type.STRING,
+        allowNull: true
+      },
+      amount:type.STRING,
+    },{
+      paranoid:true
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return queryInterface.dropTable("cards");
+
+  }
+};
