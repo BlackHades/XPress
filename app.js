f94a161c9098dbf7dda7a6f938f9c5049d4983a0
BlackHades <michealakinwonmi@gmail.com>
2019-03-27 08:34:05 +0100

    Minor Mods

--- app.js
+++ app.js
@@ -34,9 +34,9 @@ sequelize
     .then(() => {
       console.log('Connection has been established successfully.');
       //
-      sequelize.sync({force: false}).then(() => {
-          seeder();
-      });
+      // sequelize.sync({force: false}).then(() => {
+      //     seeder();
+      // });
     })
     .catch(err => {
       console.error('Unable to connect to the database:', err);
