190c68f02e141cdad54703e115a27c7e044e9a1e
BlackHades <michealakinwonmi@gmail.com>
2019-04-14 18:22:45 +0100

    Api Fix

--- helpers/Logger.js
+++ helpers/Logger.js
@@ -5,6 +5,7 @@
  * @param message
  */
 module.exports = (message) => {
+    console.debug(message)
     if(process.env.APP_ENV === "local" || process.env.APP_ENV === "development"){
         console.debug(message)
     }
