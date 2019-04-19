6aca57e3c5a5f7d3234e9e6ba04ad5fd6b4d5a11
BlackHades <michealakinwonmi@gmail.com>
2019-04-19 09:37:32 +0100

    Minor Mods

--- app.js
+++ app.js
@@ -33,10 +33,10 @@ sequelize
     .authenticate()
     .then(() => {
       console.log('Connection has been established successfully.');
-      //
-      sequelize.sync({force: false}).then(() => {
-          // seeder();
-      });
+      // //
+      // sequelize.sync({force: false}).then(() => {
+      //     // seeder();
+      // });
 
     })
     .catch(err => {
