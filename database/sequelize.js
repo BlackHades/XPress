bf6b0e8faff645883d115402a520bd123bb8ce68
Micheal Akinwonmi <michealakinwonmi@gmail.com>
2019-09-21 09:22:19 +0100

    Minor Mods

--- database/sequelize.js
+++ database/sequelize.js
@@ -4,9 +4,9 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, pr
     host: process.env.DB_HOST,
     dialect: 'mysql',
     pool: {
-        max: 10,
+        max: 100,
         min: 0,
-        acquire: 30000,
+        acquire: 100000,
         idle: 10000
     },
     define: {
