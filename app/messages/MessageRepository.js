ac931a6430b825919ab3c17930b91088413f53de
Micheal Akinwonmi <michealakinwonmi@gmail.com>
2019-09-21 14:30:03 +0100

    Minor Mods

--- app/messages/MessageRepository.js
+++ app/messages/MessageRepository.js
@@ -43,8 +43,8 @@ const fetchMessage = (userId,lastMessageId, limit) => {
           as: "bitcoin"
         }
       ],
-      order: [['id', 'ASC']]
-      // limit: limit || 50
+      order: [['id', 'ASC']],
+      limit: limit || 50
     });
 };
 
