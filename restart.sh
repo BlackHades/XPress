62bdca011b754df2bbce50b72c062f450038cbe4
Micheal Akinwonmi <michealakinwonmi@gmail.com>
2019-09-24 15:52:52 +0100

    Minor Mods

--- restart.sh
+++ restart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-PORT=8000 pm2 restart bin/www --name="8000"
-PORT=8001 pm2 restart bin/www --name="8001"
-PORT=8002 pm2 restart bin/www --name="8002"
+PORT=8000 pm2 restart 8000
+PORT=8001 pm2 restart 8001
+PORT=8002 pm2 restart 8002
