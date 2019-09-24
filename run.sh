823a22d433947f2e00b857cf1dec503be9671580
Micheal Akinwonmi <michealakinwonmi@gmail.com>
2019-09-24 21:59:20 +0100

    Minor Mods

--- run.sh
+++ run.sh
@@ -2,4 +2,7 @@
 
 PORT=8000 pm2 start bin/www --name="8000"
 PORT=8001 pm2 start bin/www --name="8001"
-#PORT=8002 pm2 start bin/www --name="8002"
+PORT=8002 pm2 start bin/www --name="8002"
+PORT=8003 pm2 start bin/www --name="8003"
+PORT=8004 pm2 start bin/www --name="8004"
+PORT=8005 pm2 start bin/www --name="8005"
