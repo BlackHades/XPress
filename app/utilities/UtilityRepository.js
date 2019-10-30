1511c0a07e0f9b2632fa1a334de54f8c89cfd095
Akinyemi Olumide Emmannuel <akinolu52@gmail.com>
2019-10-30 18:45:54 +0100

    callback

--- app/utilities/UtilityRepository.js
+++ app/utilities/UtilityRepository.js
@@ -1,16 +1,16 @@
 'use strict';
 const debug = require("debug")("app:debug");
-const {Utility} = require('../../database/sequelize');
+const { Utility } = require('../../database/sequelize');
 const cache = require("../../services/CacheManager");
 const cacheKey = "utilities";
 exports.fetchByKey = key => {
-    return Utility.findOne({where:{key:key}});
+    return Utility.findOne({ where: { key: key } });
 };
 
 
 exports.fetchAll = async () => {
     const fromCache = await cache.getAsync(cacheKey);
-    if(fromCache)
+    if (fromCache)
         return JSON.parse(fromCache);
     const utilities = await Utility.findAll();
     cache.setAsync(cacheKey, JSON.stringify(utilities));
@@ -18,16 +18,16 @@ exports.fetchAll = async () => {
 };
 
 
-exports.upsert = (key,value) => {
+exports.upsert = (key, value) => {
     cache.clear(cacheKey);
     return Utility
-        .findOne({ where: {key: key}})
-        .then(function(obj) {
-            if(obj) { // update
-                return obj.update({key:key, value:value});
+        .findOne({ where: { key: key } })
+        .then(function (obj) {
+            if (obj) { // update
+                return obj.update({ key: key, value: value });
             }
             else { // insert
-                return Utility.create({key:key, value:value});
+                return Utility.create({ key: key, value: value });
             }
         })
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,21 +2,24 @@
 const express = require('express');
 const router = express.Router();
 //Middleware
-const {authenticate, adminAuth} = require('../../app/middleware/ApiAuthMiddleware');
+const { authenticate, adminAuth } = require('../../app/middleware/ApiAuthMiddleware');
 
 //Controllers
 const contactController = require('../../app/contact-us/ContactController');
 
 //Validators
-const contactValidator  = require('../../app/contact-us/ContactValidator');
+const contactValidator = require('../../app/contact-us/ContactValidator');
 
 
 router.post('/save', contactValidator.save(), contactController.save);
 router.post('/callback', contactController.callback)
 //
 // //Admin Routes Only
-// router.use(authenticate,adminAuth);
 router.get('/all', contactController.fetch);
+
+router.use(authenticate, adminAuth);
+router.get('/callbacks', contactController.callbacks)
+
 
 module.exports = router;
 
